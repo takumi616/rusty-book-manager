11649614fc96cbf36552753d5dce01ebb6d6b385
takumi616 <ohohyeah321@icloud.com>
2024-10-07 21:57:46 +0900

    Run simple http server using axum.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::{Ipv4Addr, SocketAddr};
+
+use axum::{routing::get, Router};
+use tokio::net::TcpListener;
+
+async fn hello_world() -> &'static str {
+    "Hello, world!"
+}
+
+//this macro is used to run main func on tokio runtime.
+#[tokio::main]
+async fn main() {
+    //Set up router
+    let app = Router::new().route("/hello", get(hello_world));
+
+    //Create listener with port 8080
+    let addr = SocketAddr::new(Ipv4Addr::LOCALHOST.into(), 8080);
+    let listener = TcpListener::bind(addr).await.unwrap();
+
+    println!("Listening on {}", addr);
+
+    //Run server
+    axum::serve(listener, app).await.unwrap();
 }
