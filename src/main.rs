1c9637ba9ddeca452f658d618a69eea1d8f76cf7
takumi616 <ohohyeah321@icloud.com>
2024-10-07 23:43:17 +0900

    Implement health check endpoint.

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,27 @@
 use std::net::{Ipv4Addr, SocketAddr};
 
-use axum::{routing::get, Router};
+//anyhow::Error automatically converts error type implementing std::error::Error
+//into anyhow::Error type.
+use anyhow::Result;
+use axum::{http::StatusCode, routing::get, Router};
 use tokio::net::TcpListener;
 
-async fn hello_world() -> &'static str {
-    "Hello, world!"
+pub async fn health_check() -> StatusCode {
+    StatusCode::OK
 }
 
 //this macro is used to run main func on tokio runtime.
 #[tokio::main]
-async fn main() {
+async fn main() ->Result<()> {
     //Set up router
-    let app = Router::new().route("/hello", get(hello_world));
+    let app = Router::new().route("/health", get(health_check));
 
     //Create listener with port 8080
     let addr = SocketAddr::new(Ipv4Addr::LOCALHOST.into(), 8080);
-    let listener = TcpListener::bind(addr).await.unwrap();
+    let listener = TcpListener::bind(addr).await?;
 
     println!("Listening on {}", addr);
 
     //Run server
-    axum::serve(listener, app).await.unwrap();
+    Ok(axum::serve(listener, app).await?)
 }
