b438faedac0b4b68eee0ae9e49b654be857ef68f
takumi616 <ohohyeah321@icloud.com>
2025-01-19 12:28:56 +0900

    Introduce layered architecture.

---
+++ src/bin/app.rs
@@ -0,0 +1,34 @@
+use std::net::{Ipv4Addr, SocketAddr};
+
+use adapter::database::connect_database_with;
+use anyhow::{Error, Result};
+use api::route::health::build_health_check_routers;
+use axum::Router;
+use registry::AppRegistry;
+use shared::config::AppConfig;
+use tokio::net::TcpListener;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    bootstrap().await
+}
+
+async fn bootstrap() -> Result<()> {
+    let app_config = AppConfig::new()?;
+    let pool = connect_database_with(&app_config.database);
+    let registry = AppRegistry::new(pool);
+    let app = Router::new()
+        .merge(build_health_check_routers())
+        .with_state(registry);
+
+    //Create a new socket address from localhost and port 8080.
+    let addr = SocketAddr::new(Ipv4Addr::LOCALHOST.into(), 8080);
+
+    //Create a new TcpListener bound to the address defined above.
+    let listener = TcpListener::bind(addr).await?;
+
+    println!("Listening on {}", addr);
+
+    //Run server.
+    axum::serve(listener, app).await.map_err(Error::from)
+}
